47d819f0883ad9a5ccd68699241051ebe1f375d1
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-09-28 13:55:50 +0200

    change program name fix main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,22 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 13:49:26 by elias             #+#    #+#             */
-/*   Updated: 2023/09/28 13:49:41 by elias            ###   ########.fr       */
+/*   Updated: 2023/09/28 13:53:27 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include <iostream>
+
+int main(int argc, char **argv)
 {
+    (void)argv;
+    if (argc != 2)
+    {
+        std::cout << "\e[31m[ERROR]\e[0m Usage: ./btc <filename>" << std::endl;
+        return (1);
+    }
+
+
 
     return (0);
 }
