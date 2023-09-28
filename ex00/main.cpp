0c6bccfbdf3d468fe1055b8fa78f3e5ba9d127d4
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-09-28 15:19:54 +0200

    add open data in main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,22 +6,23 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 13:49:26 by elias             #+#    #+#             */
-/*   Updated: 2023/09/28 13:53:27 by elias            ###   ########.fr       */
+/*   Updated: 2023/09/28 15:18:43 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv)
 {
-    (void)argv;
     if (argc != 2)
     {
         std::cout << "\e[31m[ERROR]\e[0m Usage: ./btc <filename>" << std::endl;
-        return (1);
+        return (1); 
     }
+    BitcoinExchange bitcoin(argv[1]);
+    bitcoin.openDataFile("data.csv");
 
-
-
+    
     return (0);
 }
