464810a8144d10961a55499a9bb8948e12e9a832
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-10-16 16:06:26 +0200

    add convert to stack in main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:46:12 by elias             #+#    #+#             */
-/*   Updated: 2023/10/16 15:57:31 by elias            ###   ########.fr       */
+/*   Updated: 2023/10/16 16:05:50 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int main(int argc, char **argv)
     if (argc != 2)
         return (std::cerr << "Usage: ./RPN <input>" << std::endl, 1);
     RPN rpn(argv[1]);
-    
-    
-
+    rpn.convertToStack();
     return (0);
 }
