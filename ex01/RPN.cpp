cc8595976ca7503721271dc9c390e29786d1d79b
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-10-17 11:48:42 +0200

    finish ex01

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 13:34:13 by elias             #+#    #+#             */
-/*   Updated: 2023/10/16 15:55:24 by elias            ###   ########.fr       */
+/*   Updated: 2023/10/17 11:48:18 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,55 @@ void RPN::print(std::string const &str, int color) const
 		std::cout << colorsString << "[RPN " << this->_input << "] " << reset;
 	else
 		std::cout << colorsString << "[RPN " << this->_input << "] " << reset << str << std::endl;
+}
+
+// Methods
+bool RPN::_isOperator(char c)
+{
+	return (c == '+' || c == '-' || c == '/' || c == '*');
+}
+
+bool RPN::_isOverflow(double result)
+{
+	return (result > 2147483647 || result < -2147483648);
+}
+
+int	RPN::_calculate(char charOperator)
+{
+	if (this->_stack.size() < 2)
+		throw (std::runtime_error("two operands are required"));
+	double	result;
+	double	operands[2];
+	operands[0] = this->_stack.top();
+	this->_stack.pop();
+	operands[1] = this->_stack.top();
+	this->_stack.pop();
+	switch (charOperator)
+	{
+		case '*':
+			result = operands[0] * operands[1];
+			if (this->_isOverflow(result))
+				throw (std::runtime_error("overflow !"));
+			return (result);
+		case '/':
+			if (operands[1] == 0)
+				throw (std::runtime_error("division by 0"));
+			result = operands[0] / operands[1];
+			if (this->_isOverflow(result))
+				throw (std::runtime_error("overflow !"));
+			return (result);
+		case '+':
+			result = operands[0] + operands[1];
+			if (this->_isOverflow(result))
+				throw (std::runtime_error("overflow !"));
+			return (result);
+		case '-':
+			result = operands[0] - operands[1];
+			if (this->_isOverflow(result))
+				throw (std::runtime_error("overflow !"));
+			return (result);
+	}
+	return (0);
 }
 
 // Constructors
@@ -64,3 +113,38 @@ RPN::~RPN()
 }
 
 // Methods
+void RPN::convertToStack(void)
+{
+	std::istringstream	iss(this->_input);
+	std::string 		element;
+	bool				lastIsOperator = false;
+	try
+	{
+		while (getline(iss, element, ' '))
+		{
+			if (this->_isOperator(element[0]))
+			{
+				this->_stack.push(this->_calculate(element[0]));
+				lastIsOperator = true;
+			}
+			else
+			{
+				if (this->_stack.size() == 2)
+					throw (std::runtime_error("too much operands"));
+				if (!isdigit(element[0]))
+					throw (std::runtime_error("non numeric character"));
+				if (element.size() > 1)
+					throw (std::runtime_error("number sould be between 0 and 9"));
+				this->_stack.push(std::strtol(element.c_str(), NULL, 10));
+				lastIsOperator = false;
+			}
+		}
+		if (!lastIsOperator)
+			throw (std::runtime_error("last char is not an operator"));
+		std::cout << this->_stack.top() << std::endl;
+	}
+	catch(const std::exception& error)
+	{
+		std::cerr << "\e[31m[ERROR]\e[0m " << error.what() << std::endl;
+	}
+}
--- ex01/includes/RPN.hpp
+++ ex01/includes/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 13:34:13 by elias             #+#    #+#             */
-/*   Updated: 2023/10/16 15:54:05 by elias            ###   ########.fr       */
+/*   Updated: 2023/10/17 11:35:56 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,21 @@
 # define RPN_CLASS_H
 
 # include <iostream>
+# include <stack>
+# include <sstream>
+# include <stdlib.h>
 
 class RPN
 {
 	private:
-		std::string	_input;
+		std::string 		_input;
+		std::stack<double>	_stack;
 		// Print
 		void	print(std::string const &str, int color) const;
+		// Methods
+		bool	_isOperator(char c);
+		bool	_isOverflow(double result);
+		int		_calculate(char charOperator);
 
 	public:
 		// Constructors
@@ -31,6 +39,7 @@ class RPN
 		~RPN();
 
 		// Methods
+		void	convertToStack(void);
 };
 
 #endif
